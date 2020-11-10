5afef9e0ecf45e6342903523368a88211a527741
HarryAnkcorn <harry.ankcorn@gmail.com>
2020-11-10 22:05:15 +0000

    save to the bucket

--- graph_lambda/make_graph.py
+++ graph_lambda/make_graph.py
@@ -57,4 +57,6 @@ def graphing(event, context):
     plt.legend(skill_results.keys())
 
     plt.grid()
-    plt.savefig('plot.png')
+    plt.savefig('/tmp/plot.png')
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file('/tmp/plot.png', 'linkedinwebscrap', 'plot.png')
