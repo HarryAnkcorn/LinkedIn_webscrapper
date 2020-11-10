15213284a32bc0feb1fa6bb186c6e75e025a050e
HarryAnkcorn <harry.ankcorn@gmail.com>
2020-11-10 14:44:17 +0000

    get a deploy error

--- func_extract.py
+++ func_extract.py
@@ -3,7 +3,8 @@ from urllib.request import urlopen
 
 def web_scrap(url):
     uclient = urlopen(url)
-    uclient.addheaders = ('User-agent', 'Mozilla/5.0')
+    uclient.addheaders = ('User-Agent', "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36")
+    # uclient.addheaders = ('User-agent', 'Mozilla/5.0')
     page_html = uclient.read()
     uclient.close()
     page_soup = soupifie(page_html)
